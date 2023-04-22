f31586c384ec1066dad869e278dd4efda6aaf10e
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-04-22 19:57:41 +0200

    Using created lifecycle

--- src/App.vue
+++ src/App.vue
@@ -23,28 +23,21 @@ export default {
     Navbar,
     PageViewer
   },
+  created() {
+    this.getPages();
+  },
   data() {
     return {
       activePage: 0,
-      pages: [
-        {
-          link: {text: 'Home', url: 'index.html'},
-          pageTitle: 'Home Page',
-          content: 'This is home content'
-        },
-        {
-          link: {text: 'About', url: 'about.html'},
-          pageTitle: 'About Page',
-          content: 'This is about content'
-        },
-        {
-          link: {text: 'Contact', url: 'contact.html'},
-          pageTitle: 'Contact Page',
-          content: 'This is contact content'
-        }
-      ],
+      pages: []
     };
   },
+  methods: {
+    async getPages() {
+      let res = await fetch('pages.json');
+      this.pages = await res.json();
+    }
+  }
 }
 </script>
 
