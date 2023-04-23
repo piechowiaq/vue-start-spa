9fbaf9c88182055b694f5cd6463606bd8b40e075
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-04-23 20:12:09 +0200

    CreatePage with v-model functionality

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,28 @@
 
   </navbar>
 
-  <page-viewer
-      v-if="pages.length > 0"
-      :page="pages[activePage]"
-  ></page-viewer>
+<!--  <page-viewer-->
+<!--      v-if="pages.length > 0"-->
+<!--      :page="pages[activePage]"-->
+<!--  ></page-viewer>-->
+
+  <create-page
+    :page-created="pageCreated">
+
+  </create-page>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
 import PageViewer from "@/components/PageViewer";
+import CreatePage from "@/components/CreatePage";
 
 
 export default {
   components: {
     Navbar,
-    PageViewer
+    PageViewer,
+    CreatePage
   },
   created() {
     this.getPages();
@@ -38,6 +45,9 @@ export default {
       let res = await fetch('pages.json');
       let data = await res.json();
       this.pages = data;
+    },
+    pageCreated(pageObj) {
+      console.log(pageObj)
     }
   }
 }
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container mb-3">
+    <form action="">
+      <div class="mb-3">
+        <label for="" class="form-label">
+          Page Title
+        </label>
+        <input
+            type="text"
+            class="form-control"
+            v-model="pageTitle"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label">
+          Content
+        </label>
+        <textarea
+            type="text"
+            class="form-control"
+            rows="5"
+        ></textarea>
+      </div>
+      <div class="mb-3">
+        <button
+            class="btn btn-primary"
+            @click.prevent="pageCreated({pageTitle})"
+        >Create Page</button>
+
+      </div>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreatePage",
+
+  props: ['pageCreated'],
+
+  data(){
+    return {
+      pageTitle: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
