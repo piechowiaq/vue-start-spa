e2b7bceb263b3ac7b954f2a24ae3d5af6afef3f5
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-06-08 21:02:10 +0200

    Creating a form for edit page

--- src/views/PageEdit.vue
+++ src/views/PageEdit.vue
@@ -1,16 +1,84 @@
 <template>
 
-  edit {{ route.params.index }}
+
+
+  <form action="" class="container mb-3">
+    <div class="row">
+      <div class="col-md-8" >
+        <div class="mb-3">
+          <label for="" class="form-label">
+            Page Title
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="page.pageTitle"
+          >
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">
+            Content
+          </label>
+          <textarea
+            type="text"
+            class="form-control"
+            rows="5"
+            v-model="page.content"
+          >
+          </textarea>
+        </div>
+      </div>
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label">
+            Link Text
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="page.link.text"
+          >
+        </div>
+        <div class="row mb-3">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              v-model="page.published">
+            <label class="form-check-label" for="gridCheck1">
+              Published
+            </label>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    <div class="mb-3">
+      <button class="btn btn-primary me-2">
+        Edit
+      </button>
+      <button class="btn btn-secondary">
+        Cancel
+      </button>
+
+    </div>
+  </form>
 
 </template>
 
 <script setup>
 
-import { useRoute } from "vue-router";
+import { useRouter } from "vue-router";
+import { inject } from "vue";
 
-const route = useRoute();
+const router = useRouter();
 
-const props = defineProps(['index']);
+const pages = inject('$pages');
+
+const {index} = defineProps(['index']);
+
+let page = pages.getSinglePage(index);
 
 </script>
 
